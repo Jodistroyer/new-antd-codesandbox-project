5b4c914442eea354bfbc9ca989e5489729f1f4f7
Jodistroyer <91838328+Jodistroyer@users.noreply.github.com>
2022-12-14 16:35:23 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { Button, Space } from "antd";
+import AntButton from "./components/input/antd/AntButton";
+
+export default function App() {
+  return (
+    <div className="App">
+      <AntButton />
+    </div>
+  );
+}
